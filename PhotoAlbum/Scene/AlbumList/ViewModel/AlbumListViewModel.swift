0bdc2312b0a6fca26cb7c5fdd8be4f73512c6985
skyler <ho971122@gmail.com>
2023-02-06 18:33:35 +0900

    Feat: 유저앨범, 스마트앨범 데이터 모델링 및 데이터 바인딩

---
+++ PhotoAlbum/Scene/AlbumList/ViewModel/AlbumListViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  AlbumListViewModel.swift
+//  PhotoAlbum
+//
+//  Created by 이현호 on 2023/02/06.
+//
+
+import Foundation
+import Photos
+
+class AlbumListViewModel {
+    
+    var data: AlbumListDataModel?
+    var userAlbums: PHFetchResult<PHAssetCollection>!
+    var smartAlbums: PHFetchResult<PHAssetCollection>!
+    
+    init() {
+        userAlbums = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .albumRegular, options: nil)
+        smartAlbums = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil)
+    }
+    
+    func configure(type: Section, indexPath:Int) {
+        let collection = type.rawValue == 0 ? userAlbums.object(at: indexPath) : smartAlbums.object(at: indexPath)
+        let fetchAssets = PHAsset.fetchAssets(in: collection, options: nil)
+        data = AlbumListDataModel(thumbnail: fetchAssets.firstObject?.convertImage(), title: collection.localizedTitle, count: fetchAssets.count)
+    }
+}
