f32ce09e03bd83312a4a99f5149b63fca1013e34
skyler <ho971122@gmail.com>
2023-02-06 18:28:26 +0900

    Feat: Raw한 값 담을 enum 생성 및 navigationTitle 값 추가

---
+++ PhotoAlbum/Utility/Enum/TextCase.swift
@@ -0,0 +1,14 @@
+//
+//  TextCase.swift
+//  PhotoAlbum
+//
+//  Created by 이현호 on 2023/02/06.
+//
+
+import Foundation
+
+enum TextCase {
+    enum AlbumList: String {
+        case navigationTitle = "앨범"
+    }
+}
