6a393c530a6cb55920bcd84b48c8c97928204742
skyler <ho971122@gmail.com>
2023-02-06 19:36:50 +0900

    Feat: PhotoListCollectionViewCell 파일 생성

---
+++ PhotoAlbum/Scene/PhotoList/Cell/PhotoListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoListCollectionViewCell.swift
+//  PhotoAlbum
+//
+//  Created by 이현호 on 2023/02/06.
+//
+
+import UIKit
+
+import SnapKit
+
+class PhotoListCollectionViewCell: BaseCollectionViewCell {
+    
+    
+    
+}
+
