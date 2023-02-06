8a1b18bf28c8ba5ab104eeba972ba6534b114bfb
skyler <ho971122@gmail.com>
2023-02-06 20:00:27 +0900

    Feat: 앨범 내부 사진을 보여줄 PhotoListViewController 추가

---
+++ PhotoAlbum/Scene/PhotoList/ViewController/PhotoListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoListViewController.swift
+//  PhotoAlbum
+//
+//  Created by 이현호 on 2023/02/06.
+//
+
+import Foundation
+
+class PhotoListViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
