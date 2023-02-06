8ef08a661f6e0506c2c7f134acf741813761e859
skyler <ho971122@gmail.com>
2023-02-06 18:28:44 +0900

    Feat: Section enum 생성

---
+++ PhotoAlbum/Utility/Enum/Section.swift
@@ -0,0 +1,13 @@
+//
+//  Section.swift
+//  PhotoAlbum
+//
+//  Created by 이현호 on 2023/02/06.
+//
+
+import Foundation
+
+enum Section: Int, CaseIterable {
+    case userAlbums
+    case smartAlbums
+}
