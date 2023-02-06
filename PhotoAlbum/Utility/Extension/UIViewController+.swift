911e7147f869f0fdef072efba08d1897b9580b4e
skyler <ho971122@gmail.com>
2023-02-06 19:16:16 +0900

    Feat: 화면 전환 코드 Extension으로 추가

---
+++ PhotoAlbum/Utility/Extension/UIViewController+.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+.swift
+//  PhotoAlbum
+//
+//  Created by 이현호 on 2023/02/06.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    enum TransitionStyle {
+        case present
+        case push
+    }
+    
+    func transition(_ vc: UIViewController, transitionStyle: TransitionStyle) {
+        switch transitionStyle {
+        case .present:
+            self.present(vc, animated: true)
+        case .push:
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
