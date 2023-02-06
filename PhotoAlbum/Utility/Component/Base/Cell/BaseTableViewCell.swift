285965b4cd656ef8140e433e34d152794d65903d
skyler <ho971122@gmail.com>
2023-02-06 18:04:08 +0900

    Feat: TableViewCell BaseComponent 추가

---
+++ PhotoAlbum/Utility/Component/Base/Cell/BaseTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BaseTableViewCell.swift
+//  PhotoAlbum
+//
+//  Created by 이현호 on 2023/02/06.
+//
+
+import UIKit
+
+import SnapKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {}
+    
+    func setConstraints() {}
+}
