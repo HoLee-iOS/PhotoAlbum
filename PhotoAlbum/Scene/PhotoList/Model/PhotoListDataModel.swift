3163162aa35d3645a664f390858f8dbbc93a3039
skyler <ho971122@gmail.com>
2023-02-07 14:51:20 +0900

    Feat: 사진 모아보기 화면에서 사용할 데이터 모델링

---
+++ PhotoAlbum/Scene/PhotoList/Model/PhotoListDataModel.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoListDataModel.swift
+//  PhotoAlbum
+//
+//  Created by 이현호 on 2023/02/06.
+//
+
+import UIKit
+
+final class PhotoListDataModel {
+    var photo: UIImage?
+    var title: String?
+    var size: String
+    
+    init(photo: UIImage?, title: String?, size: String) {
+        self.photo = photo
+        self.title = title
+        self.size = size
+    }
+}
