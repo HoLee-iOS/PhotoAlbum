c653ea710fd1463cc4e30b252bdd8202e2d828d6
skyler <ho971122@gmail.com>
2023-02-07 14:50:53 +0900

    Feat: 사진 모아보기 화면, 경고창 화면에서 사용하는 TextCase 추가

--- PhotoAlbum/Utility/Enum/TextCase.swift
+++ PhotoAlbum/Utility/Enum/TextCase.swift
@@ -7,9 +7,23 @@
 
 import Foundation
 
-enum TextCase {
+enum TextCase: String {
+    
+    case option = "creationDate"
+    
     enum AlbumList: String {
         case empty = "0"
         case navigationTitle = "앨범"
+    }
+    
+    enum PhotoList: String {
+        case fileSize = "fileSize"
+    }
+    
+    enum Alert: String {
+        case title = "사진정보"
+        case fileName = "파일명 : "
+        case fileSize = "\n파일 크기 : "
+        case ok = "확인"
     }
 }
