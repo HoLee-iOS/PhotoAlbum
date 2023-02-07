aff7494d1b80652df0c044d2df299e5d00ec5d96
skyler <ho971122@gmail.com>
2023-02-07 14:46:47 +0900

    Feat: 시작화면 이미지 추가

--- PhotoAlbum/Resource/Image/Images.swift
+++ PhotoAlbum/Resource/Image/Images.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 enum Images {
+    
+    enum Splash: String {
+        case icon = "photo"
+    }
+    
     enum AlbumList: String {
         case arrow = "chevron.right"
         case empty = "photo.on.rectangle.angled"
